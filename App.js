ebbe4f64780816c0df6840f04f7d9311cf5c2b85
Joel Henry <62007957+joelhenry2512@users.noreply.github.com>
2023-11-30 23:09:27 +0530

    Update App.js

--- App.js
+++ App.js
@@ -9,15 +9,16 @@ export default function App() {
         return (
             <Card
                 key={item.id}
-                item={item}
+                {...item}
+                
             />
         )
     })        
     
-            // <Hero />
     return (
         <div>
             <Navbar />
+            <Hero />
             <section className="cards-list">
                 {cards}
             </section>
